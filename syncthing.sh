50f4c8908a16615342192a8e2d79bbf48884ea07
Daniel Harte <norgeous@users.noreply.github.com>
2022-03-10 18:03:43 +0000

    update

--- syncthing.sh
+++ syncthing.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 [[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
 
-curl -s https://syncthing.net/release-key.txt | apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/sources.list.d/syncthing.list
+# https://apt.syncthing.net/
+curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/sources.list.d/syncthing.list
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
 apt update
+apt install -y syncthing
 
-apt install -y syncthing xmlstarlet
+# autostart syncthing on boot with systemd
 cat <<'EOF' > /etc/systemd/system/syncthing.service
 [Unit]
 Description=Syncthing - Open Source Continuous File Synchronization
@@ -21,47 +24,20 @@ RestartForceExitStatus=3 4
 [Install]
 WantedBy=multi-user.target
 EOF
+
+# add syncthing desktop shortcut
 mkdir /usr/share/syncthing
 curl -o /usr/share/syncthing/syncthing.svg https://raw.githubusercontent.com/syncthing/syncthing/master/assets/logo-only.svg
 cat <<'EOF' > /usr/share/applications/syncthing.desktop
 [Desktop Entry]
 Name=Syncthing
 Categories=Network;Utility;
-Exec=chromium-browser http://127.0.0.1:8384/
+Exec=open http://127.0.0.1:8384/
 Icon=/usr/share/syncthing/syncthing.svg
 StartupNotify=true
 Terminal=false
 Type=Application
 EOF
+
 systemctl enable syncthing
-systemctl start syncthing
-sleep 40
-systemctl stop syncthing
-rm -r /home/pi/Sync
-mkdir /home/pi/sync
-chown pi:pi /home/pi/sync
-xmlstarlet ed --inplace -d '/configuration/folder'                                          /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/options/defaultFolderPath'     -v '~/sync'       /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/gui/theme'                     -v 'black'        /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/options/startBrowser'          -v 'false'        /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/options/natEnabled'            -v 'false'        /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/options/globalAnnounceEnabled' -v 'false'        /home/pi/.config/syncthing/config.xml
-xmlstarlet ed --inplace -u '/configuration/options/relaysEnabled'         -v 'false'        /home/pi/.config/syncthing/config.xml
-
-whiptail \
-  --title "Syncthing" \
-  --yesno "By default the Syncthing web GUI is only available on locally, should we open the GUI for access from any host?" 15 60 \
-  --yes-button "open to everyone" \
-  --no-button "open locally only" \
-  --defaultno \
-  --scrolltext \
-  && xmlstarlet ed --inplace -u '/configuration/gui/address'                -v '0.0.0.0:8384' /home/pi/.config/syncthing/config.xml
-
-ID=$(xmlstarlet sel -t -v '/configuration/device/@id' /home/pi/.config/syncthing/config.xml)
-echo
-echo "###################################################################"
-echo "# $ID #"
-echo "###################################################################"
-echo
-
 systemctl start syncthing
