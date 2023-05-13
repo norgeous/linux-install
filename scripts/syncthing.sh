6bd05b3bb1a701c3b0582d0c8430c98463c6dca5
Daniel Harte <norgeous@users.noreply.github.com>
2023-05-13 17:58:59 +0100

    integrate

--- scripts/syncthing.sh
+++ scripts/syncthing.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-[[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
+# [[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
+
+sudo apt install -y curl
 
 # https://apt.syncthing.net/
-curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+sudo curl -s -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | tee /etc/apt/sources.list.d/syncthing.list
 echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
-apt update
-apt install -y syncthing
+sudo apt update
+sudo apt install -y syncthing
 
 # autostart syncthing on boot with systemd
-cat <<'EOF' > /etc/systemd/system/syncthing.service
+sudo cat <<'EOF' > /etc/systemd/system/syncthing.service
 [Unit]
 Description=Syncthing - Open Source Continuous File Synchronization
 Documentation=man:syncthing(1)
@@ -26,9 +28,9 @@ WantedBy=multi-user.target
 EOF
 
 # add syncthing desktop shortcut
-mkdir /usr/share/syncthing
-curl -o /usr/share/syncthing/syncthing.svg https://raw.githubusercontent.com/syncthing/syncthing/master/assets/logo-only.svg
-cat <<'EOF' > /usr/share/applications/syncthing.desktop
+sudo mkdir /usr/share/syncthing
+sudo curl -o /usr/share/syncthing/syncthing.svg https://raw.githubusercontent.com/syncthing/syncthing/master/assets/logo-only.svg
+sudo cat <<'EOF' > /usr/share/applications/syncthing.desktop
 [Desktop Entry]
 Name=Syncthing
 Categories=Network;Utility;
@@ -39,5 +41,5 @@ Terminal=false
 Type=Application
 EOF
 
-systemctl enable syncthing
-systemctl start syncthing
+sudo systemctl enable syncthing
+sudo systemctl start syncthing
--- scripts/tj_n.sh
+++ scripts/tj_n.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-[[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
+# [[ $EUID -ne 0 ]] && echo "You must be running as user root." && exit 1
 
-apt install -y git build-essential
+sudo apt install -y git build-essential
 
 cd ~
 git clone https://github.com/tj/n.git
 cd n
-make install
+sudo make install
 cd ..
 rm -rf n
 
 #n lts
 #n stable
-n latest
+sudo n latest
 
-npm install -g npm
+sudo npm install -g npm
 
 node -v
 npm -v
 
-npm i -g ntl
+sudo npm i -g ntl # node task list - command line package.json menu
