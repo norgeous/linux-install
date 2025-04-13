49ffe87a2f370f908fcf5a0b8c8e882a3c0d8b1e
norgeous <norgeous@users.noreply.github.com>
2025-04-13 11:15:54 +0100

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -148,6 +148,11 @@ for i in $CHOICES; do
 
   # pyenv pyhton version manager
   # curl -fsSL https://pyenv.run | bash
+  # echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+  # echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+  # echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
+  # sudo apt update
+  # sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl git libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
   if [[ "$i" == '"VSCODE"' ]]; then
     sudo snap install code --classic
