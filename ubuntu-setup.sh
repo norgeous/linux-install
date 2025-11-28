d6eacf9d1dd0451e31c6819612db8cf235993f56
Daniel Harte <norgeous@users.noreply.github.com>
2025-11-28 13:50:08 +0000

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -161,7 +161,7 @@ for i in $CHOICES; do
   if [[ "$i" == '"MPV"' ]]; then
     sudo apt install -y mpv
     mkdir -p ~/.config/mpv/
-    echo "r playlist-shuffle\nup add volume 1\ndown add volume -1" > ~/.config/mpv/input.conf
+    echo -e "r playlist-shuffle\nup add volume 1\ndown add volume -1" > ~/.config/mpv/input.conf
     echo -e "volume=50\nloop-playlist=inf" > ~/.config/mpv/mpv.conf
   fi
 
